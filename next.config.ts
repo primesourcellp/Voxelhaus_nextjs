edae3fb0470d594b07e20ceb3c7f5e19e0141291
sramkumar2k2-cmd <sramkumar2k2@gmail.com>
2025-12-02 10:54:28 +0530

    .

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,22 @@
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  /* config options here */
+  
+  trailingSlash: true,
+
+  images: {
+    unoptimized: false, 
+    remotePatterns: [
+      { protocol: "https", hostname: "i0.wp.com" },
+      { protocol: "https", hostname: "voxelhausconsultancy.com" },
+    ],
+  },
+
+  reactStrictMode: true,
+
+  turbopack: {
+    root: __dirname,
+  },
 };
 
 export default nextConfig;
